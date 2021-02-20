d6c1615e6463aef6f39c53c8d27bd1b3fed0f1d2
Iryna Zharnosek <i.zharnosek@gmail.com>
2021-02-20 22:12:02 +0200

    Update index.js

--- HW-1/index.js
+++ HW-1/index.js
@@ -12,20 +12,20 @@ const priceMin = Math.min(priceProduct1,priceProduct2,priceProduct3);
 console.log(priceMin);
 
 // sum() Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму 
-const priceSum = priceProduct1+priceProduct2+priceProduct3;
-console.log( priceSum);
+const priceSum = priceProduct1 + priceProduct2 + priceProduct3;
+console.log(priceSum);
 
 //  Math.floor() Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. Округлення використовувати в МЕНЬШУ сторону.
 const priceProduct1Floor = Math.floor(priceProduct1);
 const priceProduct2Floor = Math.floor(priceProduct2);
 const priceProduct3Floor = Math.floor(priceProduct3);
-console.log(priceProduct1Floor+", "+priceProduct2Floor+", "+priceProduct3Floor);
+console.log(priceProduct1Floor + ", " + priceProduct2Floor + ", " + priceProduct3Floor);
 
-const priceSumFloor = priceProduct1Floor+priceProduct2Floor+priceProduct3Floor;
-console.log( priceSumFloor);
+const priceSumFloor = priceProduct1Floor + priceProduct2Floor + priceProduct3Floor;
+console.log(priceSumFloor);
 
 //Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
-const priceSumRoundingPlus  = Math.round(priceSumFloor/100)*100;
+const priceSumRoundingPlus  = Math.round(priceSumFloor / 100) * 100;
 console.log(priceSumRoundingPlus);
 
 //Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
@@ -33,21 +33,23 @@ function isEven(number) {
     return number % 2 === 0; 
 }
 const priceSumInt = isEven(priceSumFloor);
-console.log( priceSumInt)
+console.log(priceSumInt)
 
 //Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
-const remainderSum = 500-priceSum;
+const payment = 500;
+const remainderSum = (payment - priceSum).toFixed(2);
 console.log(remainderSum)
 
 //Виведіть середнє значення цін, округлене до другого знаку після коми
-const averageValue = (priceSum/3).toFixed(2);
+const averageValue = (priceSum / 3).toFixed(2);
 console.log(averageValue)
 
 //Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random). 
 const discount = Math.floor(Math.random() * 100) + 1;
-const invoice = (priceProduct2-discount).toFixed(2);
-const netProfit = (priceProduct2/2-(priceProduct2-invoice)).toFixed(2);
+const invoice = (priceProduct2Floor - discount).toFixed(2);
+const netProfit = (((priceProduct2Floor / 2) - discount)).toFixed(2);
 console.log("discount: "+ discount +"\r\n"+ "invoice: "+invoice+"\r\n"+ "Profit: "+netProfit)
+
 
 console.log(`Максимальна ціна: ${priceMax}
 Мінімальна ціна: ${priceMin}
@@ -58,7 +60,7 @@ console.log(`Максимальна ціна: ${priceMax}
 Решта при оплаті 500 грн.: ${remainderSum}
 Середня ціна округлена до сотих: ${averageValue}
 Випадкова знижка: ${discount}
-Ціна продукту: ${priceProduct2}
+Ціна продукту: ${priceProduct2Floor}
 Рахунок зі знижкою: ${invoice}
 Чистий прибуток: ${netProfit}`
 )
@@ -76,6 +78,3 @@ console.log(`Максимальна ціна: ${priceMax}
                 <br/>Рахунок зі знижкою: ${invoice}
                 <br/>Чистий прибуток: ${netProfit}`
  )
-// document.write("<br/>Максимальна ціна: " + priceMax )
-// document.write("<br/>Максимальна ціна: " + priceMax )
-// document.write("<br/>Максимальна ціна: " + priceMax )
